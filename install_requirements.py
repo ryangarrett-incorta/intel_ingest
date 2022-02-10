f7c93489172efbf031002061d768e65bd12caac3
ryangarret-incorta <91205018+ryangarret-incorta@users.noreply.github.com>
2022-02-09 19:25:26 -0500

    1

--- install_requirements.py
+++ install_requirements.py
@@ -7,32 +7,27 @@ import sys
 import platform
 import shutil
 import time
+import glob
 
-incorta_home = os.getenv('INCORTA_HOME')
-egg_file = (os.path.join("./incorta_synapse-*.egg"))
+# incorta_home = os.getenv('INCORTA_HOME')
+# egg_file = glob.glob('./*.egg')
+# egg_file_str = egg_file[0]
+# egg_file_path = (os.path.join(incorta_home, "IncortaNode/incorta.ml/lib"))
 
-osType = platform.dist()
-osDist = osType[0]
-osVersion = osType[1] 
-os_number = osVersion[0] 
-#repoPath = (os.path.join("/etc/yum.repos.d/mssql-release.repo"))
+# fileYum = "yum_requirements.txt"
+# fileObj = open(fileYum)
+# yumPackages = fileObj.read().splitlines()
+# fileObj.close()
 
-fileYum = "yum_requirements.txt"
-fileObj = open(fileYum)
-yumPackages = fileObj.read().splitlines()
-fileObj.close()
-
-filePip = "pip_requirements.txt"
-fileObj = open(filePip)
-pipPackages = fileObj.read().splitlines()
-fileObj.close()
-
-print(egg_file)
+# filePip = "pip_requirements.txt"
+# fileObj = open(filePip)
+# pipPackages = fileObj.read().splitlines()
+# fileObj.close()
 
 # subprocess.call(["pip", "install", "--upgrade", "pip"])
 # subprocess.call(["sudo", "yum", "remove", "unixODBC-utf16-devel"])
 # subprocess.call(["sudo", "ACCEPT_EULA=Y", "yum", "install", "-y", "msodbcsql17"])
-#subprocess.call(["sudo", "su", "-"])
+# subprocess.call(["sudo", "su", "-"])
 # time.sleep(2)
 # print("sleeping")
 # subprocess.call(["curl", "https://packages.microsoft.com/config/rhel/7/prod.repo" ">" "/etc/yum.repos.d/mssql-release.repo"])
@@ -45,8 +40,14 @@ print(egg_file)
 # for p in pipPackages:
 #     subprocess.call(["python3", "-m", "pip", "install", p])
 
-# shutil.copy(source_file, l)
-#         print("Copied " + source_file + " to " + l)
+# shutil.copy(egg_file_str,egg_file_path)
+# print("Copied " + egg_file_str + " to " + egg_file_path)
+
+# osType = platform.dist()
+# osDist = osType[0]
+# osVersion = osType[1] 
+# os_number = osVersion[0] 
+#repoPath = (os.path.join("/etc/yum.repos.d/mssql-release.repo"))
 
 # if osDist == "centos":
 #     repo_url = 'https://packages.microsoft.com/config/centos/' + os_number + '/prod.repo'
